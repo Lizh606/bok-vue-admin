7553dece0927e6df2e35af981d5e8ec8e302a5a6
李泽航 <lizh@dist.com.cn>
2025-01-23 13:21:05 +0800

    fix: 新增userInfo接口以及路由控制

--- src/views/User/User.vue
+++ src/views/User/User.vue
@@ -1,25 +1,44 @@
 <template>
   <div class="tw-flex tw-flex-col tw-gap-4">
     <div>
-      <el-button type="primary" round @click="openDialog('add')">新增</el-button>
+      <el-button type="primary" round @click="openDialog('add')"
+        >新增</el-button
+      >
     </div>
-    <div class="tw-flex-1 tw-min-h-0" ref="tableRef">
+    <div class="tw-min-h-0 tw-flex-1" ref="tableRef">
       <el-table :data="tableData" stripe border :height="tableHeight">
         <el-table-column prop="id" label="id" width="80" align="center" />
-        <el-table-column prop="username" label="用户名" width="180" align="center" />
+        <el-table-column
+          prop="username"
+          label="用户名"
+          width="180"
+          align="center"
+        />
         <el-table-column label="角色" width="220" align="center">
           <template #default="scope">
             <div class="tw-flex tw-w-full tw-items-center tw-justify-center">
-              <span>{{ scope.row.roles.map((i: Role) => i.name).join('、') }}</span>
+              <span>{{
+                scope.row.roles.map((i: Role) => i.name).join("、")
+              }}</span>
             </div>
           </template>
         </el-table-column>
-        <el-table-column prop="gender" label="性别" width="180" align="center" />
+        <el-table-column
+          prop="gender"
+          label="性别"
+          width="180"
+          align="center"
+        />
         <el-table-column prop="address" label="Address" />
         <el-table-column label="操作">
           <template #default="scope">
-            <el-button size="small" @click="openDialog('edit', scope.row)">编辑</el-button>
-            <el-button size="small" type="danger" @click="openDialog('delete', scope.row)"
+            <el-button size="small" @click="openDialog('edit', scope.row)"
+              >编辑</el-button
+            >
+            <el-button
+              size="small"
+              type="danger"
+              @click="openDialog('delete', scope.row)"
               >删除</el-button
             >
           </template>
@@ -51,57 +70,61 @@
 </template>
 
 <script setup lang="ts">
-import useTablePagination from '@/hooks/useTablePagination'
-import { getUserListByPage, type Role, type User } from '@/services'
-import { onMounted, ref } from 'vue'
-import { USER_DIALOG_TITLE } from './common'
-import DeleteDialog from './dialogs/DeleteDialog.vue'
-import OperateDialog from './dialogs/OperateDialog.vue'
-// table元素
-const tableRef = ref()
-// table高度
-const tableHeight = ref()
+  import useTablePagination from "@/hooks/useTablePagination"
+  import { getUserListByPage, type Role, type User } from "@/services"
+  import { onMounted, ref } from "vue"
+  import { USER_DIALOG_TITLE } from "./common"
+  import DeleteDialog from "./dialogs/DeleteDialog.vue"
+  import OperateDialog from "./dialogs/OperateDialog.vue"
+  // table元素
+  const tableRef = ref()
+  // table高度
+  const tableHeight = ref()
 
-const FormatData = (data: User[]) => {
-  return data.map((i: User) => {
-    return { ...i.profile, ...i }
-  })
-}
-const { tableData, pageRef, pageSizeRef, getTableData, totalRef } = useTablePagination<User>(
-  getUserListByPage,
-  { queryParams: { page: 1, size: 10 }, FormatData, immediate: true }
-)
-const addDialog = ref(false)
-const deleteDialog = ref(false)
-const dialogTitle = ref(USER_DIALOG_TITLE.ADD)
-const currentFormData = ref()
-const openDialog = (type: string, data?: any) => {
-  if (data) {
-    currentFormData.value = data
+  const FormatData = (data: User[]) => {
+    return data.map((i: User) => {
+      return { ...i.profile, ...i }
+    })
   }
-  switch (type) {
-    case 'add':
-      dialogTitle.value = USER_DIALOG_TITLE.ADD
-      addDialog.value = true
-      break
-    case 'edit':
-      dialogTitle.value = USER_DIALOG_TITLE.EDIT
-      addDialog.value = true
-      break
-    case 'delete':
-      deleteDialog.value = true
-      break
-    default:
-      break
+  const { tableData, pageRef, pageSizeRef, getTableData, totalRef } =
+    useTablePagination<User>(getUserListByPage, {
+      queryParams: { page: 1, size: 10 },
+      FormatData,
+      immediate: true
+    })
+  const addDialog = ref(false)
+  const deleteDialog = ref(false)
+  const dialogTitle = ref(USER_DIALOG_TITLE.ADD)
+  const currentFormData = ref()
+  const openDialog = (type: string, data?: any) => {
+    if (data) {
+      currentFormData.value = data
+    } else {
+      currentFormData.value = null
+    }
+    switch (type) {
+      case "add":
+        dialogTitle.value = USER_DIALOG_TITLE.ADD
+        addDialog.value = true
+        break
+      case "edit":
+        dialogTitle.value = USER_DIALOG_TITLE.EDIT
+        addDialog.value = true
+        break
+      case "delete":
+        deleteDialog.value = true
+        break
+      default:
+        break
+    }
   }
-}
 
-onMounted(() => {
-  tableHeight.value = tableRef.value.offsetHeight
-  window.onresize = () => {
+  onMounted(() => {
     tableHeight.value = tableRef.value.offsetHeight
-  }
-})
+    window.onresize = () => {
+      tableHeight.value = tableRef.value.offsetHeight
+    }
+  })
 </script>
 
 <style lang="scss"></style>
