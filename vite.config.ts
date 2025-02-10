6931802b21e3b6ea3f4e7d991aadb16835ce07db
李泽航 <lizh@dist.com.cn>
2025-02-10 14:22:34 +0800

    build: 修改为/admin/

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import Components from "unplugin-vue-components/vite"
 import { defineConfig } from "vite"
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/admin",
+  base: "/admin/",
   plugins: [
     vue(),
     AutoImport({
